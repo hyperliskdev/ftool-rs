c0a713d5a36d9e1cd045aec3138cec331ed99159
Carter Tomlenovich <cartertomlenovich@gmail.com>
2025-07-31 16:27:38 -0400

    added cleaner logging and file log

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
+use chrono::Local;
 use clap::{Parser, Subcommand};
-use rusty_falcon::{
-    easy::client::{FalconHandle},
-};
-use std::{path::PathBuf};
+use fern::Dispatch;
+use log::{debug, error};
+use rusty_falcon::easy::client::FalconHandle;
+use std::{fs, path::PathBuf};
 use tools::tag_hosts::tag_hosts;
 extern crate dotenv;
 
 use dotenv::dotenv;
 mod tools;
+
+fn setup_logger() -> Result<(), Box<dyn std::error::Error>> {
+     // Ensure logs folder exists
+    fs::create_dir_all("logs")?;
+
+    // Log file name based on timestamp
+    let log_file_name = format!("logs/run_{}.log", Local::now().format("%Y-%m-%d_%H-%M-%S"));
+    let log_file = fern::log_file(log_file_name)?;
+
+    Dispatch::new()
+        // Global level filter
+        .level(log::LevelFilter::Trace)
+        // Console: show everything
+        .chain(
+            Dispatch::new()
+                .level(log::LevelFilter::Trace)
+                .format(|out, message, record| {
+                    out.finish(format_args!("[{}] {}", record.level(), message))
+                })
+                .chain(std::io::stdout()),
+        )
+        // File: only Warn and above
+        .chain(
+            Dispatch::new()
+                .level(log::LevelFilter::Warn)
+                .format(|out, message, record| {
+                    out.finish(format_args!(
+                        "{} [{}] {}",
+                        Local::now().format("%Y-%m-%d %H:%M:%S"),
+                        record.level(),
+                        message
+                    ))
+                })
+                .chain(log_file),
+        )
+        .apply()?;
+
+    Ok(())
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -34,54 +74,72 @@ enum Commands {
         /// The hosts to tag
         hosts: Option<PathBuf>,
 
-        #[arg(long, value_name = "ACTION", default_value="add")]
+        #[arg(long, value_name = "ACTION", default_value = "add")]
         action: String,
     },
 }
 
 #[tokio::main]
 async fn main() {
-
     dotenv().ok();
+    // Setup logger
+    setup_logger().unwrap_or_else(|e| {
+        error!("Failed to set up logger: {}", e);
+        std::process::exit(1);
+    });
 
+    // Parse command line arguments
     let cli = Cli::parse();
 
     let falcon = FalconHandle::from_env()
-        .await
-        .expect("Could not authenticate with CrowdStrike API", );
+        .await.unwrap_or_else(|e| {
+            error!("Failed to create Falcon client: {}", e);
+            std::process::exit(1);
+        });
 
     match &cli.command {
         Some(Commands::TagHosts { tag, hosts, action }) => {
-            
             // Check if a file exists and pass the path to the tag_hosts function
             let hosts = match hosts {
                 Some(path) => {
                     if path.exists() {
                         path.clone()
                     } else {
-                        eprintln!("The specified file does not exist: {:?}", path);
+                        debug!("The specified file does not exist: {:?}", path);
                         return;
                     }
                 }
                 None => {
-                    eprintln!("No hosts file provided. Please specify a file with the --hosts option.");
+                    debug!(
+                        "No hosts file provided. Please specify a file with the --hosts option."
+                    );
                     return;
                 }
             };
-            
-            let result = tag_hosts(
-                &falcon,
-                tag.clone(),
-                hosts,
-                action.clone(),
-            )
-            .await
-            .expect("Failed to tag hosts");
 
-            println!("{}", result);
+            let result = tag_hosts(&falcon, tag.clone(), hosts, action.clone()).await;
+
+            if let Err(errors) = result {
+                for error in errors {
+                    error!("Error tagging hosts: {:?}", error);
+                }
+                return;
+            }
+
+            if action == "remove" {
+                for host in &result.unwrap_or_default() {
+                    debug!("Host with tag: {:?} removed from {:?}", tag, host.device_id);
+                }
+            } else {
+                for host in &result.unwrap_or_default() {
+                    debug!("Host tagged: {:?} with tag(s): {:?}", host, tag);
+                }
+            }
+
+            debug!("Hosts tagged successfully");
         }
         None => {
-            eprintln!("No command provided. Use --help for more information.");
+            error!("No command provided. Use --help for more information.");
         }
     }
 }
--- src/tools/tag_hosts.rs
+++ src/tools/tag_hosts.rs
@@ -1,22 +1,22 @@
-use std::{path::{PathBuf}};
+use std::path::PathBuf;
 
+use log::{error, info};
 use rusty_falcon::{
-    apis::{
-        hosts_api::{query_devices_by_filter, update_device_tags},
-    },
+    apis::hosts_api::{query_devices_by_filter, update_device_tags},
     easy::client::FalconHandle,
-    models::DeviceapiPeriodUpdateDeviceTagsRequestV1,
+    models::{DeviceapiPeriodUpdateDeviceDetailsResponseV1, DeviceapiPeriodUpdateDeviceTagsRequestV1, MsaspecPeriodError},
 };
+
 
 pub async fn tag_hosts(
     falcon: &FalconHandle,
     tag: Vec<String>,
     hosts: PathBuf,
     action: String,
-) -> Result<String, Box<dyn std::error::Error>> {
+) -> Result<Vec<DeviceapiPeriodUpdateDeviceDetailsResponseV1> , Vec<MsaspecPeriodError>> {
     // Read the file containing hostname.
     // The file is expected to contain one hostname per line.
-    let hostnames: Vec<String> = std::fs::read_to_string(&hosts)?
+    let hostnames: Vec<String> = std::fs::read_to_string(&hosts).expect("Failed to read hosts file")
         .lines()
         .map(|line| line.trim().to_string())
         .filter(|line| !line.is_empty())
@@ -30,13 +30,22 @@ pub async fn tag_hosts(
             .collect::<Vec<_>>()
             .join(",")
     );
-    println!("Filter: {}", filter);
+    
+    info!("Querying devices with filter: {}", filter);
+
+
     let host_ids =
-        query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str()))
-            .await?;
+        query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str())).await.map_err(|e| {
+            error!("Failed to query devices by filter: {}", e);
+            vec![MsaspecPeriodError {
+                code: 500,
+                id: None,
+                message: e.to_string(),
+            }]
+        })?;
 
     if !host_ids.errors.is_empty() {
-        return Err(format!("Error querying devices by filter: {:?}", host_ids.errors).into());
+        return Err(host_ids.errors);
     }
 
     let update_tag = update_device_tags(
@@ -47,14 +56,19 @@ pub async fn tag_hosts(
             tags: tag,
         },
     )
-    .await?;
+    .await.map_err(|e| {
+        error!("Failed to update device tags: {}", e);
+        vec![MsaspecPeriodError {
+            code: 500,
+            id: None,
+            message: e.to_string(),
+        }]
+    })?;
 
-    if let Some(errors) = &update_tag.errors {
-        return Err(format!("Error updating device tags: {:?}", errors).into());
+    if let Some(ref errors) = update_tag.errors {
+        error!("Errors occurred while updating device tags: {:?}", errors);
+        return Err(errors.clone());
     }
 
-
-    Ok(
-        format!("Successfully updated tags for hosts: {:?}", update_tag.resources).into()
-    )
+    Ok(update_tag.resources)
 }
