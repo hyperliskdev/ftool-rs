b669792f5e0120f240e719664b77f6491e8fc968
Carter Tomlenovich <cartertomlenovich@gmail.com>
2025-09-26 11:33:51 -0400

    Some fricken error bs

--- src/tools/alive_hosts.rs
+++ src/tools/alive_hosts.rs
@@ -4,14 +4,14 @@ use log::{error, info, warn};
 use rusty_falcon::{
     apis::hosts_api::{get_device_details_v2, query_devices_by_filter},
     easy::client::FalconHandle,
-    models::{DeviceapiPeriodDeviceDetailsResponseSwagger, MsaspecPeriodError},
+    models::{DeviceapiPeriodDeviceDetailsResponseSwagger},
 };
 
 // Take in a list of hostnames and find them in crowdstrike, return ones that are not found in crowdstrike.
 pub async fn alive_hosts(
     falcon: &FalconHandle,
     hosts: PathBuf,
-) -> Result<DeviceapiPeriodDeviceDetailsResponseSwagger, Vec<MsaspecPeriodError>> {
+) -> Result<DeviceapiPeriodDeviceDetailsResponseSwagger, Box<dyn std::error::Error>> {
     let hostnames: Vec<String> = std::fs::read_to_string(&hosts)
         .expect("Failed to read hosts file")
         .lines()
@@ -32,8 +32,13 @@ pub async fn alive_hosts(
 
     info!("Querying devices for sensor_ids.");
 
-    let host_ids =
-        query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str())).await.;
+    let host_ids = match query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str())).await {
+        Ok(hosts) => hosts,
+        Err(e) => {
+            error!("Error querying devices: {:?}", e);
+            return Err(Box::new(e));
+        }
+    };
 
     let hosts = get_device_details_v2(&falcon.cfg, host_ids.resources).await?;
 
