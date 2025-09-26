ef5101f9784f3da3bce7429fb43b2e38f5c22cec
Carter Tomlenovich <cartertomlenovich@gmail.com>
2025-09-26 11:08:25 -0400

    fix something please

--- src/tools/alive_hosts.rs
+++ src/tools/alive_hosts.rs
@@ -2,18 +2,16 @@ use std::path::PathBuf;
 
 use log::{error, info, warn};
 use rusty_falcon::{
-    apis::{
-        hosts_api::{get_device_details_v2, query_devices_by_filter},
-    },
+    apis::hosts_api::{get_device_details_v2, query_devices_by_filter},
     easy::client::FalconHandle,
-    models::{MsaspecPeriodError},
+    models::{DeviceapiPeriodDeviceDetailsResponseSwagger, MsaspecPeriodError},
 };
 
 // Take in a list of hostnames and find them in crowdstrike, return ones that are not found in crowdstrike.
 pub async fn alive_hosts(
     falcon: &FalconHandle,
     hosts: PathBuf,
-) -> Result<(), Vec<MsaspecPeriodError>> {
+) -> Result<DeviceapiPeriodDeviceDetailsResponseSwagger, Vec<MsaspecPeriodError>> {
     let hostnames: Vec<String> = std::fs::read_to_string(&hosts)
         .expect("Failed to read hosts file")
         .lines()
@@ -35,19 +33,9 @@ pub async fn alive_hosts(
     info!("Querying devices for sensor_ids.");
 
     let host_ids =
-        query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str())).await.map_err(|e| {
-            error!("Failed to query devices by filter: {}", e);
-            vec![MsaspecPeriodError {
-                code: 500,
-                id: None,
-                message: e.to_string(),
-            }]
-        })?;
+        query_devices_by_filter(&falcon.cfg, None, None, None, Some(filter.as_str())).await.;
 
-    let hosts = get_device_details_v2(&falcon.cfg, host_ids.resources).await;
+    let hosts = get_device_details_v2(&falcon.cfg, host_ids.resources).await?;
 
-    info!("{:?}", hosts);
-    
-
-    Ok(())
+    Ok(hosts)
 }
